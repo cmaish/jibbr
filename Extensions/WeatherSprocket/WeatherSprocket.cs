a2e57d0eb96bb99ef7042b28094dff6271e25b91
Stefan D. Poulsen <cyberzed@sleddog.dk>
2012-01-13 23:23:11 +0100

    #39 Tested and found working...comitted and push for comparison

---
+++ Extensions/WeatherSprocket/WeatherSprocket.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using Jabbot.CommandSprockets;
+
+namespace WeatherSprocket
+{
+	public class WeatherSprocket : CommandSprocket
+	{
+		public override IEnumerable<string> SupportedInitiators
+		{
+			get { yield return "weather"; }
+		}
+
+		public override IEnumerable<string> SupportedCommands
+		{
+			get { yield return "93063"; }
+		}
+
+		public override bool ExecuteCommand()
+		{
+			Bot.Say("Temp: 64F", Message.Receiver);
+
+			return true;
+		}
+	}
+}
