ed07e016d177f3ef6a4128f2cf637c387957696c
Stefan D. Poulsen <cyberzed@sleddog.dk>
2012-05-10 21:33:55 +0200

    Ported volunteer sprocket

--- Extensions/VolunteerSprocket/VolunteerSprocket.cs
+++ Extensions/VolunteerSprocket/VolunteerSprocket.cs
@@ -1,39 +1,38 @@
 ﻿using System;
-using System.Diagnostics;
 using System.Linq;
 using System.Text.RegularExpressions;
+using JabbR.Client.Models;
 using Jabbot.Core;
-using Jabbot.Core.Models;
 using Jabbot.Core.Sprockets;
 
 namespace VolunteerSprocket
 {
-    public class VolunteerSprocket : RegexSprocket
-    {
-        public override Regex Pattern
-        {
-            get { return new Regex(@"[-_./""\w\s]*volunteer some[-_./""\w\s]*"); }
-        }
+	public class VolunteerSprocket : RegexSprocket
+	{
+		public override Regex Pattern
+		{
+			get { return new Regex(@"[-_./""\w\s]*volunteer some[-_./""\w\s]*"); }
+		}
 
-        public override void ProcessMatch(Match match, ChatMessage chatMessage, IBot bot)
-        {
-            Debug.WriteLine("Volunteering!");
-            if (chatMessage.Content.StartsWith(bot.Name) || chatMessage.Content.StartsWith("@" + bot.Name))
-            {
-                var users = bot.GetUsers(chatMessage.Room).Result.Where(c => c != bot.Name).ToList();
+		protected override void ProcessMatch(Match match, IBot bot, Message message, string room)
+		{
+			if (message.Content.StartsWith(bot.Name) || message.Content.StartsWith(string.Format("@{0}", bot.Name)))
+			{
+				var users = bot.GetUsers(room).Result.Where(c => c != bot.Name).ToList();
 
-                if(!users.Any())
-                {
-                    bot.Send("Bot, you can't tell yourself to do that", chatMessage.Room);
-                    return;
-                }
+				if (!users.Any())
+				{
+					bot.Send("Bot, you can't tell yourself to do that", room);
 
-                var random = new Random();
+					return;
+				}
 
-                var randomUser = random.Next(0, users.Count() - 1);
+				var random = new Random();
 
-                bot.Send(string.Format("I volunteer {0} for that!", users[randomUser]), chatMessage.Room);
-            }
-        }
-    }
+				var randomUser = random.Next(0, users.Count - 1);
+
+				bot.Send(string.Format("I volunteer {0} for that!", users[randomUser]), room);
+			}
+		}
+	}
 }
