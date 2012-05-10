cf632ee4a7ceb671aaac37ade975eefb8bc8f75a
Stefan D. Poulsen <cyberzed@sleddog.dk>
2012-05-10 21:39:48 +0200

    Started CommandSprocket porting

--- Jabbot.CommandSprockets/ICommandSprocket.cs
+++ Jabbot.CommandSprockets/ICommandSprocket.cs
@@ -1,21 +1,21 @@
 ﻿using System.Collections.Generic;
+using JabbR.Client.Models;
 using Jabbot.Core;
-using Jabbot.Core.Models;
 using Jabbot.Core.Sprockets;
 
 namespace Jabbot.CommandSprockets
 {
-    public interface ICommandSprocket : ISprocket
-    {
-        string[] Arguments { get; }
-        IBot Bot { get; }
-        string Command { get; }
-        bool ExecuteCommand();
-        bool HasArguments { get; }
-        string Intitiator { get; }
-        bool MayHandle(string initiator, string command);
-        ChatMessage ChatMessage { get; }
-        IEnumerable<string> SupportedCommands { get; }
-        IEnumerable<string> SupportedInitiators { get; }
-    }
+	public interface ICommandSprocket : ISprocket
+	{
+		string[] Arguments { get; }
+		IBot Bot { get; }
+		string Command { get; }
+		bool HasArguments { get; }
+		string Intitiator { get; }
+		Message ChatMessage { get; }
+		IEnumerable<string> SupportedCommands { get; }
+		IEnumerable<string> SupportedInitiators { get; }
+		bool ExecuteCommand();
+		bool MayHandle(string initiator, string command);
+	}
 }
